0ffd57510b2d956c20e0ba8fcb4ccc8f555549da
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2018-12-07 14:16:03 +0900

    Created SnippetView for Retrieve, Update, Delete

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -22,3 +22,30 @@ def snippet_list(request):
       serializer.save()
       return JsonResponse(serializer.data, status=201)
     return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+  """
+  Retrieve, update or delete a code snippet.
+  """
+  try:
+    snippet = Snippet.objects.get(pk=pk)
+  except Snippet.DoesNotExist:
+    return HttpResponse(status=404)
+
+  if request.method == 'GET':
+    serializer = SnippetSerializer(snippet)
+    return JsonResponse(serializer.data)
+
+  elif request.method == 'PUT':
+    data = JSONParser().parse(request)
+    serializer = SnippetSerializer(snippet, data=data) # updateのときは、現在値と受け取ったデータを渡す
+    if serializer.is_valid():
+      serializer.save()
+      return JsonResponse(serializer.data)
+    return JsonResponse(serializer.errors, status=400)
+
+  elif request.method == 'DELETE':
+    snippet.delete()
+    return HttpResponse(status=204)
+
