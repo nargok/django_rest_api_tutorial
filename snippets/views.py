a0bfe80d70eff3f49eeddd555b1feb9c03b2088e
nargok <nargok@gmail.com>
2018-12-08 22:08:56 +0900

    Enable to edit and delete only your own snippet.

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 from django.contrib.auth.models import User
 
@@ -16,7 +17,10 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-  permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+  # 自分がownerのsnippetだけ、編集・削除できる
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly)
 
 class UserList(generics.ListAPIView):
   queryset = User.objects.all()
