5015f322b675ca60649e3cecba6391de9dea6604
nargok <nargok@gmail.com>
2018-12-08 21:41:42 +0900

    Put Login functionality on API tool

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,3 +20,8 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
