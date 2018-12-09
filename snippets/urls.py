d51d4f9b9df94aa4e075f4ef9e86f12b32a5ec55
nargok <nargok@gmail.com>
2018-12-09 11:32:02 +0900

    [Refactoring]Use ViewSet for Snippets API

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from snippets.views import UserViewSet
+from snippets.views import SnippetViewSet, UserViewSet
+
+snippet_list = SnippetViewSet.as_view({
+  'get': 'list',
+  'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+  'get': 'retrieve',
+  'put': 'update',
+  'patch': 'partial_update',
+  'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+  'get': 'highlight'
+})
 
 user_list = UserViewSet.as_view({
   'get': 'list'
@@ -12,9 +26,9 @@ user_detail = UserViewSet.as_view({
 
 urlpatterns = [
   url(r'^$', views.api_root),
-  url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-  url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-  url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+  url(r'^snippets/$', snippet_list, name='snippet-list'),
+  url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+  url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
   url(r'^users/$', user_list, name='user-list'),
   url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import generics, permissions, renderers
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -11,21 +11,23 @@ from snippets.permissions import IsOwnerOrReadOnly
 
 from django.contrib.auth.models import User
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+  """
+  This viewset automatically provides list, create, retrieve,
+  update, destroy actions.
+  """
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-  permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly,)
+
+  @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+  def highlight(self, request, *args, **kwargs):
+    snippet = self.get_object()
+    return Response(snippet.highlighted)
 
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Snippet.objects.all()
-  serializer_class = SnippetSerializer
-
-  # 自分がownerのsnippetだけ、編集・削除できる
-  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                        IsOwnerOrReadOnly)
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
   """
