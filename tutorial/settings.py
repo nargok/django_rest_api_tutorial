8fb775ad76e11e34eb35156086a55f56b5f5b29c
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2018-12-10 20:17:41 +0900

    Activate JWT Authentication

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -125,9 +125,13 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+}
+
+JWT_AUTH = {
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }
