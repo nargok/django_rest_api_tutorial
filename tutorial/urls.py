611f9d9d191768014f2daa1fdcc6a9055ff889fd
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2018-12-10 19:53:49 +0900

    Use Django REST Framework-jwt

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
+    url(r'^obtain_jwt_token', obtain_jwt_token),
 ]
 
 urlpatterns += [
