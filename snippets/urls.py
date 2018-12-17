56362ed3a339179df9b7f9e9e4e5ec3f57db5f8b
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2018-12-17 19:39:12 +0900

    ManyToManyRelationship Snippet and Tag

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,6 +6,7 @@ from snippets import views
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
+router.register(r'tags', views.TagViewSet)
 
 urlpatterns = [
   url(r'^', include(router.urls))
--- snippets/views.py
+++ snippets/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.models import Snippet, Tag
+from snippets.serializers import SnippetSerializer, UserSerializer, TagSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
 from django.contrib.auth.models import User
@@ -28,6 +28,11 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
+
+class TagViewSet(viewsets.ModelViewSet):
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
   """
