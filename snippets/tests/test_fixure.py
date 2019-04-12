3881217809e8c7efe66e956439eb6d43b106ce39
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-12 12:03:06 +0900

    fixure update

--- snippets/tests/test_fixure.py
+++ snippets/tests/test_fixure.py
@@ -1,9 +1,13 @@
 import pytest
 
-@pytest.fixture()
+# scopeをつけると１回だけ実行される
+@pytest.fixture(scope="module")
 def setup_tuple():
   global array
   array = [1, 2, 3]
+  print("テストはじめるよ∩( ・ω・)∩")
+  yield
+  print("テストおわったよ(｀･ω･´)")
 
 def test_passing(setup_tuple):
   assert array[1] == 2
