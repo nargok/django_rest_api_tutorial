ecb3c037dd52c9760721dc744dc3beb49c83ecb9
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-12 10:35:31 +0900

    smoke test

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,9 +1,10 @@
 import pytest
 
-@pytest.mark.skip("Do not execute!")
+@pytest.mark.Smoke
 def test_passing():
     assert (1, 2, 3) == (1, 2, 3)
 
+@pytest.mark.skip("Do not execute!")
 def test_skip():
     assert (1, 2, 3) == (1, 2, 3)
 
@@ -11,4 +12,8 @@ def test_skip():
 a = 101
 @pytest.mark.skip(a > 100, reason = "aが100を超えたらskipします")
 def test_skip_with_condition():
+    assert (1, 2, 3) == (1, 2, 3)
+
+@pytest.mark.Smoke
+def test_smoke2():
     assert (1, 2, 3) == (1, 2, 3)
