b70535dfa377820f98cbf5a25a99a477524dcc8a
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-12 10:27:42 +0900

    条件付きスキップ

--- snippets/tests.py
+++ snippets/tests.py
@@ -4,5 +4,11 @@ import pytest
 def test_passing():
     assert (1, 2, 3) == (1, 2, 3)
 
-def test_passing2():
+def test_skip():
+    assert (1, 2, 3) == (1, 2, 3)
+
+# 条件つきでスキップする
+a = 101
+@pytest.mark.skip(a > 100, reason = "aが100を超えたらskipします")
+def test_skip_with_condition():
     assert (1, 2, 3) == (1, 2, 3)
