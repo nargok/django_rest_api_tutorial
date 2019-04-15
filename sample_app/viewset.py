3ffc9b2043a4ea5b19bcad7d18e9862223366a57
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-15 18:02:03 +0900

    change authentication class for UserViewSet

--- sample_app/viewset.py
+++ sample_app/viewset.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, permissions
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
 
@@ -7,5 +7,4 @@ class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
   permission_classes = (permissions.IsAuthenticated,)
-  # TODO認証クラスを変更する
-  authentication_classes = (JSONWebTokenAuthentication,)
+  authentication_classes = (JWTAuthentication,)
